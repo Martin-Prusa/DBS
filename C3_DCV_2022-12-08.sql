5bf3d4c77b066dfd8269bd269954f83137a1cdeb
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-12-11 17:34:23 +0100

    x

--- C3_DCV_2022-12-08.sql
+++ C3_DCV_2022-12-08.sql
@@ -1,12 +1,12 @@
 -- 1. Zvířata, která nemá nikdo rád 76
 SELECT Z.jmeno
 FROM Zvirata AS Z LEFT JOIN Ma_Rad AS M ON M.druh = Z.druh
-WHERE ISNULL(M.id);
+WHERE M.id IS NULL;
 
 -- 2. Ošetřovatelé, kteří nemají nikoho rádi 39
 SELECT Ote.jmeno
 FROM Osetrovatele AS Ote LEFT JOIN Ma_Rad AS M ON M.osetrovatel = Ote.id
-WHERE ISNULL(M.id);
+WHERE M.id IS NULL;
 
 -- 3. Nenávistní lenoši (ošetřovatelé, kteří nemají nikoho rádi a navíc nikoho neošetřují).
 -- Note: Nevím, jestli tam nějaký takový je. Pokud ne, vytvořte si nového ošetřovatele (třeba "Lenoch Nenávistný"). Ten určitě nebude nikoho ošetřovat, ani nemá nikoho rád.
@@ -14,4 +14,4 @@ WHERE ISNULL(M.id);
 SELECT Ote.jmeno, Ote.id
 FROM Osetrovatele AS Ote LEFT JOIN Osetruje AS Oje ON Oje.osetrovatel = Ote.id
 						LEFT JOIN Ma_Rad AS M ON M.osetrovatel = Ote.id
-WHERE ISNULL(M.id) AND ISNULL(Oje.id);
+WHERE M.id IS NULL AND Oje.id IS NULL;
