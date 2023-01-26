3b465a6a2db9dc5a48d7de8cd2e6005f700889e8
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2023-01-26 13:46:10 +0100

    r

--- C3_HOD_2023-01-26.sql
+++ C3_HOD_2023-01-26.sql
@@ -11,3 +11,11 @@ FROM Zvirata AS Z LEFT JOIN
 (Osetrovatele AS Ote JOIN Osetruje AS Oje ON Oje.osetrovatel = Ote.id AND Ote.jmeno LIKE "Juhani %")
 ON Z.id = Oje.id
 WHERE Oje.id IS NULL;
+
+-- Osetrovatele neosetrujici kone - 457
+
+SELECT Ote.jmeno
+FROM Osetrovatele AS Ote LEFT JOIN (Osetruje AS Oje 
+						JOIN Zvirata AS Z ON Z.id = Oje.zvire
+                        JOIN Druhy AS D ON Z.druh = D.id AND D.nazev LIKE "kun") ON Oje.osetrovatel = Ote.id
+WHERE Oje.id IS NULL;
