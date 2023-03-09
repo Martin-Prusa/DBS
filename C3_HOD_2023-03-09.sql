7c1fa40343928b08f4632bc6cee5d318b1f4db67
Martin Průša <prusamartin.pm@gmail.com>
2023-03-09 13:31:12 +0100

    X1

--- C3_HOD_2023-03-09.sql
+++ C3_HOD_2023-03-09.sql
@@ -1,12 +1,10 @@
 -- ==== X ====
 -- 1. Ošetřovatele, kteří ošetřují dvě (nebo více) zvířat se shodným datem narození
-SELECT Ote.jmeno
-FROM Osetrovatele AS Ote
-WHERE Ote.id IN (SELECT DISTINCT Oje.osetrovatel AS osetrovatel
-                 FROM Osetruje AS Oje
-                          JOIN Zvirata AS Z ON Oje.zvire = Z.id
-                 GROUP BY Z.narozen, Oje.osetrovatel
-                 HAVING COUNT(Z.id) > 1);
+SELECT DISTINCT Ote.jmeno
+FROM Osetruje AS Oje JOIN Zvirata AS Z ON Oje.zvire = Z.id
+JOIN Osetrovatele AS Ote ON Ote.id = Oje.osetrovatel
+GROUP BY Z.narozen, Oje.osetrovatel
+HAVING COUNT(Z.id) > 1;
 
 -- 2. Nejplodnější den (den, kdy se narodilo nejvíce zvířat - chce se pouze datum!)
 
@@ -21,7 +19,13 @@ HAVING COUNT(Z.id) = (SELECT COUNT(Z.id)
 
 -- 3. Nejstarší nemilované zvíře (chce se POUZE JMÉNO!)
 SELECT Z.jmeno
-FROM Zvirata AS Z LEFT JOIN Ma_rad AS M ON Z.druh = M.druh
+FROM Zvirata AS Z
+         LEFT JOIN Ma_rad AS M ON Z.druh = M.druh
 WHERE M.id IS NULL
 ORDER BY Z.narozen
 LIMIT 1;
+
+-- ==== Y ====
+-- 1. Pro každého ošetřovatele vypište nejstarší zvíře, které daný ošetřovatel NEošetřuje
+-- 2. Pro každého ošetřovatele vypište počet zvířat, které daný ošetřovatel neošetřuje, ale má je rád
+-- 3. Data, v nichž se narodila pouze zvířata (tedy nějaké zvíře, ale žádný ošetřovatel)
