9d567871da7296174a30ee40211eefd7bdb4bbb6
Martin Průša <prusamartin.pm@gmail.com>
2023-04-26 18:31:57 +0200

    discord

--- C3_DISCORD_2023-04-26.sql
+++ C3_DISCORD_2023-04-26.sql
@@ -1,5 +1,19 @@
--- 1. Všechny ošetřovatele, kteří neošetřují současně slepici a lasičku (tzn. nemusí ošetřovat nic z toho, ale pokud ano, nesmí ošetřovat zvířata z obou druhů současně).
-
+-- 1. Všechny ošetřovatele, kteří neošetřují současně slepici a lasičku (tzn. nemusí ošetřovat nic z toho, ale pokud ano, nesmí ošetřovat zvířata z obou druhů současně). -> 497
+SELECT Ote.id, Ote.jmeno
+FROM Osetrovatele Ote
+WHERE Ote.id NOT IN (SELECT OteSlepic.Ote
+                     FROM (SELECT Ote.id AS Ote
+                           FROM Osetrovatele Ote
+                                    JOIN Osetruje Oje ON Oje.osetrovatel = Ote.id
+                                    JOIN Zvirata Z ON Oje.zvire = Z.id
+                                    JOIN Druhy D ON D.id = Z.druh
+                           WHERE D.nazev LIKE 'slepice') AS OteSlepic
+                              JOIN (SELECT Ote.id AS Ote
+                                    FROM Osetrovatele Ote
+                                             JOIN Osetruje Oje ON Oje.osetrovatel = Ote.id
+                                             JOIN Zvirata Z ON Oje.zvire = Z.id
+                                             JOIN Druhy D ON D.id = Z.druh
+                                    WHERE D.nazev LIKE 'lasicka') AS OteLasicek ON OteSlepic.Ote = OteLasicek.Ote);
 
 -- 2. Všechny nadprůměrné druhy ( = takové druhy, jejichž váhový průměr je vyšší, než průměr váhových průměrů všech druhů). -> 57
 SELECT D.id, D.nazev, prumery.prumernaVaha
