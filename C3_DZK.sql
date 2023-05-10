457fe69f78f78c3bf7b2f4c7898bde362058c29a
Martin Průša <prusamartin.pm@gmail.com>
2023-05-10 19:27:40 +0200

    dzk init

---
+++ C3_DZK.sql
@@ -0,0 +1,87 @@
+-- 1. Vypište jména ošetřovatelů, kteří ošetřují alespoň dvě zvířata od stejného druhu.
+
+-- 2. Vypište jména ošetřovatelů, kteří ošetřují alespoň dva druhy takové, že příslušný ošetřovatel
+-- ošetřuje alespoň dvě zvířata takového druhu.
+-- Pro lepší pochopení: Žádá se totéž co ve cvičení č. 1, akorát takových druhů musí ošetřovatel
+-- ošetřovat více.
+
+-- 3. Vypište takové ošetřovatele, kteří neošetřují žádné zvíře starší než on sám.
+
+-- 4. Vypište zvířata, která nikdo neošetřuje.
+
+-- 5. Vypište data, kdy se narodilo nějaké zvíře, ale žádný ošetřovatel.
+
+-- 6. Pro každý druh vypište jméno nejtěžšího a nejlehčího zvířete
+
+-- 7. Vypište všechna zvířata, která mají váhu nižší, než je průměrná váha druhu, ke kterému
+-- náleží
+
+-- 8. Ke každému ošetřovateli vypište nejmladší zvíře, které má rád a které je starší než on sám
+
+-- 9. Vypište nejošetřovávanější zvíře. Pokud je takových zvířat více, zajímají nás všechna.
+
+-- 10. Ke každému ošetřovateli vypište nejmladší ze zvířat, které ošetřuje a které je starší než
+-- příslušný ošetřovatel.
+
+-- 11. Vypište všechny ošetřovatele, kteří ošetřují všechny zajíce.
+-- Pokud by ošetřovatel měl rád i další zvířata jiných druhů, není to na škodu; podstatné je, že
+-- má rád všechny zajíce.
+
+-- 12. Vypište ošetřovatele, který nemá rád žádné ze zvířat, které ošetřuje (zvířata, která
+-- neošetřuje, rád mít může).
+
+-- 13. Vypište ošetřovatele, který má rád všechna zvířata, která ošetřuje (pokud má rád i nějaké
+-- další, která neošetřuje, nevadí to).
+
+-- 14. Vypište všechna zvířata, která nemá rád ošetřovatel Juhani Lorenc a jsou těžší než 100.
+
+-- 15. Vypište všechna zvířata, která ošetřují pouze děti (tzn. všichni ošetřovatelé musí být ročník
+-- 2005 a mladší).
+-- Poznámka: Lidem narozeným v r. 2004 letos 18 bylo nebo teprve bude. Protože funkce pro
+-- manipulaci s datem a časem nebyly letos probrány, ročník 2004 pro jednoduchost
+-- zanedbáme a budeme osoby narozené v r. 2004 posuzovat všechny jako již dospělé.
+
+-- 16. Všechny milovníky starých zvířat (takové ošetřovatele, kteří jsou mladší než každé zvíře,
+-- které mají rádi)
+
+-- 17. Všechna zvířata, která neošetřuje nikdo, jehož jméno obsahuje písmeno „X“
+
+-- 18. Všechny přemnožené druhy ( = druhy, jejichž populace je větší, než průměrná populace
+-- druhu)
+
+-- 19. Všechny ošetřovatele, kteří neošetřují staré holuby
+
+-- 20. Všechna zvířata, která mají rádi pouze staří ošetřovatelé (narození v roce 1950 a starší)
+
+-- 21. “Prodatelná” zvířata
+-- (nejmladší zvíře každého druhu, s výjimkou těch, kde by populace po prodeji klesla pod 10
+-- jedinců)
+
+-- 22. Všechny ošetřovatele, kteří neošetřují žádné zvíře, které mají rádi
+
+-- 23. Všechny hladomory
+-- Def: Hladomorem nazveme takového ošetřovatele, který ošetřuje alespoň jedno zvíře, které je nejlehčí
+-- svého druhu
+
+-- 24. Nadprůměrně těžké druhy
+-- Vahou druhu chápeme váhový průměr všech jeho zvířat. Nadprůměrně těžké druhy jsou takové druhy,
+-- jejichž váha je vyšší než průměr vah všech druhů. Druhy, které nemají žádné zvíře, do výpočtů
+-- nezahrnujte.
+
+-- 25. Všechny ošetřovatele, kteří ošetřují pouze pštrosy.
+
+-- 26. Vyhynulé druhy (nemají žádné zvíře)
+
+-- 27. Všechna zvířata, která nemá nikdo rád
+
+-- 28. Všechny ošetřovatele, kteří nikoho neošetřují
+
+-- 29. Všechny kachny, které nemá nikdo rád
+
+-- 30. Všechny ošetřovatele, kteří neošetřují kachny
+
+-- 31. Všechny ošetřovatele, kteří neošetřují kachny ani husy
+
+-- 32. Všechny ošetřovatele, kteří neošetřují těžké osly
+
+-- 33. Všechny těžké osly, které nikdo neošetřuje
